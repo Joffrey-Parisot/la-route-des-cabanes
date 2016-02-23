3cf381450e31e086e138c139def32cca079e42a0
Joffrey Parisot <joffrey.parisot2@gmail.com>
2016-02-23 02:03:55 +0100

    Effet clic

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,1 +1,21 @@
 $(document).foundation();
+
+$('.window').click(function(e){
+
+    e.preventDefault();
+
+    var placeholder = $('<div class="window__placeholder"></div>');
+
+    placeholder.css({
+        top: $(this).outerHeight(true) + 'px',
+        width: $(this).outerWidth() + 'px',
+        height: $(this).outerHeight() + 'px'
+    });
+
+    placeholder.prependTo($(this).parent());
+
+    setTimeout(function() {
+        placeholder.addClass('window__placeholder--loading');
+    }, 25);
+
+});
