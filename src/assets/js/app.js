12da6c5b5980276e1dd23913cefdf3606eca1ce5
Joffrey Parisot <joffrey.parisot2@gmail.com>
2016-03-21 02:07:02 +0200

    Chargement page presque ok

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -18,42 +18,67 @@ var support = { transitions: Modernizr.csstransitions },
             onEndCallbackFn();
         }
     },
+    urlToLoad,
     pageLanding = $('.landing'),
-    pageDummy = $('.page-dummy');
+    pageDummy = $('#page-dummy');
+
 
 $('.window').click(function(e){
 
     e.preventDefault();
 
-    var page = $(this).attr('href');
-    console.log(page);
+    // Get the clicked window
+    var window = $(this);
+    console.log(window);
 
+    // Get the url to load
+    urlToLoad = window.attr('href');
+    console.log(urlToLoad);
+
+    // Create the window placeholder
     var placeholder = $('<div class="window__placeholder"></div>');
 
     placeholder.css({
-        top: $(this).outerHeight(true) + 'px',
-        width: $(this).outerWidth() + 'px',
-        height: $(this).outerHeight() + 'px'
+        width: window.outerWidth() + 'px',
+        height: window.outerHeight() + 'px'
     });
 
-    placeholder.prependTo($(this).parent());
+    // Animate texts
+    var windowSuptitle = window.find('.window__suptitle');
+    var windowTitle = window.find('.window__title');
+    var windowSubtitle = window.find('.window__subtitle');
 
-    setTimeout(function() {
-        placeholder.addClass('window__placeholder--loading');
-    }, 25);
+    windowSuptitle.addClass('window__suptitle--loading');
+    windowTitle.addClass('window__title--loading');
+    windowSubtitle.addClass('window__subtitle--loading');
 
-    onEndTransition(placeholder[0], function() {
-        console.log('stop transition');
+    // At the end of texts animations
+    onEndTransition(windowSuptitle[0], function() {
+        console.log('stop windowSuptitle transition');
 
-        pageDummy.addClass('page-dummy--show');
+        // Add the window placeholder
+        placeholder.appendTo(window);
 
-        $.ajax({
-            url: page,
-            method: 'GET',
-            dataType: 'html',
-            success: pageLoaded
+        // And animate it
+        setTimeout(function() {
+            placeholder.addClass('window__placeholder--loading');
+        }, 150);
+
+        // At the end of the window placeholder animation
+        onEndTransition(placeholder[0], function() {
+            console.log('stop placeholder transition');
+
+            // Show the dummy page
+            pageDummy.addClass('page-dummy--show');
+
+            // And load the new page
+            $.ajax({
+                url: urlToLoad,
+                method: 'GET',
+                dataType: 'html',
+                success: pageLoaded
+            });
         });
-
     });
 
 });
@@ -61,24 +86,64 @@ $('.window').click(function(e){
 
 function pageLoaded(data){
 
-    // Get the new Page Wrapper from Ajax
+    console.log('page loaded');
+
+    // Change the browser history
+    var stateObj = { page: urlToLoad };
+    history.pushState(stateObj, document.title, urlToLoad);
+
+    // Get the new page wrapper from Ajax
     var pageWrapper = $(data).find('.page-wrapper');
 
-    // Hide the Landing block
+    // Hide the landing block
     pageLanding.addClass('page-wrapper--hide');
 
-    // Add the new Page Wrapper after the Landing block
+    // Add the new page wrapper after the landing block
     pageLanding.after(pageWrapper);
 
-    // TODO
-    // ajouter une window qui fait l'effet inverse de celle qui s'agrandit
-    // Cette window sera la placeholder de la vraie
-    // La vraie est celle en taille normal de la page qui vient de l'ajax
-    // On cache le dummy et on rétrécit la nouvelle window placeholder pour enfin voir la nouvelle page
+    // Get the big window
+    var bigWindow = pageWrapper.find('#big-window');
 
+    // Create the big window placeholder
+    var bigPlaceholder = $('<div class="window__placeholder window__placeholder--loading"></div>');
 
-    // Hide the Dummy
-    //pageDummy.removeClass('page-dummy--show');
+    bigPlaceholder.css({
+        width: bigWindow.outerWidth() + 'px',
+        height: bigWindow.outerHeight() + 'px'
+    });
 
+    bigPlaceholder.appendTo(bigWindow);
+
+    // Hide the dummy page
+    setTimeout(function() {
+        pageDummy.removeClass('page-dummy--show');
+    }, 150);
+
+    // At the end of the dummy page animation
+    onEndTransition(pageDummy[0], function() {
+        console.log('stop dummy page transition');
+
+        // Animate the big window placeholder
+        setTimeout(function() {
+            bigPlaceholder.removeClass('window__placeholder--loading');
+        }, 150);
+    });
+
+    // At the end of the big window placeholder animation
+    onEndTransition(bigPlaceholder[0], function() {
+        console.log('stop big placeholder transition');
+
+        // Fade it
+        bigPlaceholder.addClass('window__placeholder--shrink');
+
+        // At the end of the big window placeholder animation again
+        onEndTransition(bigPlaceholder[0], function() {
+            console.log('stop big placeholder transition again');
+
+            // Hide it
+            bigPlaceholder.addClass('window__placeholder--hide');
+            bigPlaceholder.removeClass('window__placeholder--shrink');
+        });
+    });
 
 }
