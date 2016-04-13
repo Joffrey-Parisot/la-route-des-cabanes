4703cd869acfb22764b6a62c4097bf7ddbc83dd1
Joffrey Parisot <joffrey.parisot2@gmail.com>
2016-04-14 01:25:21 +0200

    Animations Ajax

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -23,7 +23,7 @@ var support = { transitions: Modernizr.csstransitions },
     pageDummy = $('#page-dummy');
 
 
-$('.window').click(function(e){
+$('a.window').click(function(e){
 
     e.preventDefault();
 
@@ -101,8 +101,18 @@ function pageLoaded(data){
     // Add the new page wrapper after the landing block
     pageLanding.after(pageWrapper);
 
-    // Get the big window
+    // Get the big window and its children
     var bigWindow = pageWrapper.find('#big-window');
+    var bigSupTitle = bigWindow.find('.window__suptitle');
+    var bigTitle = bigWindow.find('.window__title');
+    var bigSubTitle = bigWindow.find('.window__subtitle');
+    var bigText = bigWindow.find('.window__text');
+
+    // Hide children
+    bigSupTitle.addClass('window__suptitle--loading');
+    bigTitle.addClass('window__title--loading');
+    bigSubTitle.addClass('window__subtitle--loading');
+    bigText.addClass('window__text--loading');
 
     // Create the big window placeholder
     var bigPlaceholder = $('<div class="window__placeholder window__placeholder--loading"></div>');
@@ -143,6 +153,12 @@ function pageLoaded(data){
             // Hide it
             bigPlaceholder.addClass('window__placeholder--hide');
             bigPlaceholder.removeClass('window__placeholder--shrink');
+
+            // And show children
+            bigSupTitle.removeClass('window__suptitle--loading');
+            bigTitle.removeClass('window__title--loading');
+            bigSubTitle.removeClass('window__subtitle--loading');
+            bigText.removeClass('window__text--loading');
         });
     });
 
